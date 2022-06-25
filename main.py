b34d601ce4dcea0f07254d3630ad7839a192048f
Hayder Al-Chalabi <96077191+hay90der@users.noreply.github.com>
2022-06-25 12:10:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from colorama import Fore, Back, Style
 from IPython.display import Markdown, display
 import seaborn as sns
